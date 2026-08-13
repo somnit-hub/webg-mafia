86837818253e5ce8050020d48cc579b81d0ad0fa
Konstantin <somnit3d@gmail.com>
2026-08-14 01:05:38 +0300

    Зробити таймери двоколонковими на мобільному

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mafia-desk-v129';
+const CACHE = 'mafia-desk-v130';
 const APP_SHELL = [
   './', './index.html', './styles.css', './manifest.webmanifest',
   './assets/logo-mafia.webp', './assets/favicon-32.png', './assets/favicon-64.png', './assets/favicon.ico',
